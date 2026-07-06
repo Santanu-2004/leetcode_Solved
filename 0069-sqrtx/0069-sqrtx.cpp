c348ce50d13e63d01f3c9b9d5511ea896bbebe91
Santanu Maiti <146585281+Santanu-2004@users.noreply.github.com>
2026-07-06 19:59:05 +0530

    Time: 0 ms (100%), Space: 8.5 MB (87%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int left = 1;
+        int right = x;
+        while (left <= right){
+            int mid = left + (right - left) / 2;
+            long long square = 1LL * mid * mid;
+            if(square == x){
+                return mid;
+            }
+            else{
+                if(square > x){
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
+                }
+            }
+        }
+        return left - 1;
+    }
+};
